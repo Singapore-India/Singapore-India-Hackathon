6358a1d21ea30c7173fc4e3ce42803643894f25e
p1yu5h0 <piyush200205@gmail.com>
2023-07-15 12:19:23 +0530

    added daily check-in

--- app/screens/Budget/BudgetScreen.tsx
+++ app/screens/Budget/BudgetScreen.tsx
@@ -1,7 +1,7 @@
 import { Pedometer } from "expo-sensors";
 
-import React ,{useState,useEffect}from "react";
-import { ScrollView,FlatList,View ,Text, StyleSheet, TouchableOpacity} from "react-native";
+import React, { useState, useEffect } from "react";
+import { ScrollView, FlatList, View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { useSelector } from "react-redux";
 import { useNavigation } from "@react-navigation/native";
 import { Button } from "components";
@@ -18,22 +18,18 @@ import axios from "axios";
 import { ScreenWidth } from "react-native-elements/dist/helpers";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-
 import Leaderboard from "./LeaderBoard";
 
 const leaderboardData = [
-  { name: 'John', score: 100 },
-  { name: 'Jane', score: 80 },
-  { name: 'Bob', score: 70 },
-  { name: 'Alice', score: 90 },
+  { name: "John", score: 100 },
+  { name: "Jane", score: 80 },
+  { name: "Bob", score: 70 },
+  { name: "Alice", score: 90 },
 ];
 
-
 const BudgetScreen: NavStatelessComponent = () => {
-
   const navigation = useNavigation();
   const navigator = navigate(navigation);
-
 
   const monthlyCarbonBudget = useSelector(selectors.getMonthlyCarbonBudget);
 
@@ -70,51 +66,66 @@ const BudgetScreen: NavStatelessComponent = () => {
   const [coins, setCoins] = useState(0);
   const [couponCount, setCouponCount] = useState(0);
 
-  
   const fetchCoins = async () => {
     // let result  = await axios.get("https://v1.nocodeapi.com/harshmr/fit/rneUryXiRdGlqISd/aggregatesDatasets?dataTypeName=steps_count")
     // console.log(result.data.steps_count[0].value );
     // setStepCount(result.data.steps_count[0].value);
     let user;
-    AsyncStorage.getItem("userName").then((res) => {
-      console.log('res',res);
-      user = res;
-      axios.post("http://10.1.156.61:8000/api/user/getcoins/",{
-      "username": user,
-    }).then((res) => {
-      console.log(res.data.coins);
-      setCoins(res.data.coins);
-      axios.post("http://10.1.156.61:8000/api/user/getcoupons/",{
-      "username": user,
-      }).then((res) => {
-        console.log(res.data);
-        setCouponCount(res.data.coupons.length);
-      }).catch((err) => {
+    AsyncStorage.getItem("userName")
+      .then((res) => {
+        console.log("res", res);
+        user = res;
+        axios
+          .post("http://10.1.156.61:8000/api/user/getcoins/", {
+            username: user,
+          })
+          .then((res) => {
+            console.log(res.data.coins);
+            setCoins(res.data.coins);
+            axios
+              .post("http://10.1.156.61:8000/api/user/getcoupons/", {
+                username: user,
+              })
+              .then((res) => {
+                console.log(res.data);
+                setCouponCount(res.data.coupons.length);
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+            // setCompleted(true);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+        // console.log(user);
+      })
+      .catch((err) => {
         console.log(err);
       });
-      // setCompleted(true);
-    }).catch((err) => {
-      console.log(err);
-    })
-      // console.log(user);
-    }).catch((err) => {
-      console.log(err);
-    });
-    
-  }
+  };
 
   useEffect(() => {
     fetchCoins();
   }, [couponCount]);
 
-  const signInHistory = [
-  
-    { day: 'Wed', signedIn: true, consecutiveDays: 3 },
-    { day: 'Thu', signedIn: false, consecutiveDays: 0 },
-    { day: 'Fri', signedIn: true, consecutiveDays: 1 },
-    { day: 'Sat', signedIn: true, consecutiveDays: 2 },
-    { day: 'Sun', signedIn: true, consecutiveDays: 3 },
-  ];
+  const [signInHistory, setSignInHistory] = useState([]);
+  const [today, setToday] = useState("");
+
+  const getCurrentDate = () => {
+    const today1 = new Date();
+    console.log("Todays Day" + today1);
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const currentDay = days[today1.getDay()];
+    setToday(currentDay);
+    return currentDay;
+  };
+
+
+  useEffect(() => {
+    updateSignInHistory();
+  }, []);
+
 
   var challenges = [
     { name: "Walk challenge 🏃‍♂️", term: 1000 },
@@ -123,124 +134,180 @@ const BudgetScreen: NavStatelessComponent = () => {
     // { name: "Eat Fruits", term: ['fruits'] }
   ];
 
- var randomChallenge = challenges[Math.floor(Math.random() * challenges.length)];
-  
- let props = randomChallenge;
-  
+  var randomChallenge = challenges[Math.floor(Math.random() * challenges.length)];
+
+  let props = randomChallenge;
+
   // Display the selected challenge
   console.log("Challenge: " + randomChallenge.name);
   console.log("Accepted Term: " + randomChallenge.term);
-  
-  
+
+  const updateSignInHistory = () => {
+    const currentDate = getCurrentDate();
+    const updatedHistory = signInHistory.slice(0, 4); // Keep only the last 4 days
+
+    const todaySignIn = {
+      day: currentDate,
+      signedIn: true,
+      consecutiveDays: 0,
+    };
+
+    if (updatedHistory.length > 0) {
+      const lastDay = updatedHistory[0].day;
+
+      if (currentDate === lastDay) {
+        todaySignIn.consecutiveDays = updatedHistory[0].consecutiveDays + 1;
+      }
+    }
+
+    updatedHistory.unshift(todaySignIn);
+
+    setSignInHistory(updatedHistory);
+  };
+
+
+  const handleCheckIn = () => {
+    const updatedHistory = signInHistory.map((item) => {
+      if (item.signedIn) {
+        return { ...item, consecutiveDays: item.consecutiveDays + 1 };
+      } else {
+        return { ...item, signedIn: true, consecutiveDays: 0 };
+      }
+    });
+
+    const currentDayIndex = updatedHistory.findIndex((item) => item.day === getCurrentDate());
+    const previousDayIndex = (currentDayIndex + 6) % 7;
+    const currentStreak = updatedHistory[previousDayIndex].consecutiveDays;
+
+    updatedHistory[currentDayIndex].consecutiveDays = currentStreak;
+
+    setSignInHistory(updatedHistory);
+  };
 
 
   const renderSignInStatus = (signedIn, consecutiveDays) => {
     let containerStyle = styleDailyCheckin.crossContainer;
-    let textContent = '✖';
-  
+    let textContent = "✖";
+
     if (signedIn) {
       containerStyle = styleDailyCheckin.tickContainer;
-      textContent = '✔';
+      textContent = "✔";
     }
-  
+
     return (
       <View>
-
-      <View style={[styleDailyCheckin.statusContainer, containerStyle]}>
-        <Text style={styleDailyCheckin.statusText}>{textContent}</Text>
-        
-      </View>
-      { (
-         <View >
-          <Text style={styleDailyCheckin.checkInPoints}>{consecutiveDays} ⭐</Text>
-  
-        </View>
+        <TouchableOpacity onPress={handleCheckIn}>
+          <View style={[styleDailyCheckin.statusContainer, containerStyle]}>
+            <Text style={styleDailyCheckin.statusText}>{textContent}</Text>
+          </View>
+        </TouchableOpacity>
+        {signedIn && (
+          <View>
+            <Text style={styleDailyCheckin.checkInPoints}>{consecutiveDays} ⭐</Text>
+            {/* {consecutiveDays > 0 && (
+              <Text style={styleDailyCheckin.streak}>Streak: {consecutiveDays} days</Text>
+            )} */}
+          </View>
         )}
       </View>
     );
   };
-  
-  
-
-  
-  
 
   return (
     <ScrollView style={styles.container}>
-
-<View style={styleDailyCheckin.superContainer}>
-
-<Text style={styleDailyCheckin.Daily}>Daily Check-in 📅</Text>
-<View style={styleDailyCheckin.container}>
-  
-      {signInHistory.map((item, index) => (
-        <View key={index} style={styleDailyCheckin.dayContainer}>
-          <View  style={sty.container}>
-
-          {/* <Text style={styleDailyCheckin.day}>{item.day}</Text> */}
-          {renderSignInStatus(item.signedIn, item.consecutiveDays)}
+       <View style={styleDailyCheckin.superContainer}>
+        <Text style={styleDailyCheckin.Daily}>Daily Check-in 📅</Text>
+        <View style={styleDailyCheckin.container}>
+          {signInHistory.slice(0, 1).map((item, index) => (
+            <View key={index} style={styleDailyCheckin.dayContainer}>
+              <Text style={styleDailyCheckin.day}>{item.day}</Text>
+              {renderSignInStatus(item.signedIn, item.consecutiveDays)}
+            </View>
+          ))}
+          
         </View>
+        {signInHistory.length > 0 && (
+          <Text style={styleDailyCheckin.finalStreak}>
+            Logged In: {signInHistory[0].consecutiveDays} times today
+          </Text>
+        )}
+        <TouchableOpacity
+            style={style.button}
+            // onPress={() => {navigator.openChallenges()(props={randomChallenge})}}
+            onPress={() => {
+              updateSignInHistory();
+            }}
+          >
+            <Text style={style.buttonText}>Check-In Now</Text>
+
+          </TouchableOpacity>
+      </View>
+
+      <View style={[style.cardContainer, { backgroundColor: "#rgba(51, 153, 102, 0.7)" }]}>
+        <Text style={style.cardTitle}>Daily Challenge</Text>
+        <View style={style.cardDivider} />
+        <View style={style.container}>
+          <Text style={style.challengeText}>{randomChallenge.name}</Text>
+          <TouchableOpacity
+            style={style.button}
+            // onPress={() => {navigator.openChallenges()(props={randomChallenge})}}
+            onPress={() => {
+              console.log("props", props);
+              navigation.navigate("Challenge", {
+                screen: "Challenge",
+                params: props,
+              });
+            }}
+          >
+            <Text style={style.buttonText}>Accept Challenge</Text>
+          </TouchableOpacity>
         </View>
-      ))}
-    </View>
-
-    </View>
-
-
-<View style={[style.cardContainer, { backgroundColor: '#rgba(51, 153, 102, 0.7)' }]}>
-      <Text style={style.cardTitle}>Daily Challenge</Text>
-      <View style={style.cardDivider} />
-      <View style={style.container}>
-        <Text style={style.challengeText}>{randomChallenge.name}</Text>
-        <TouchableOpacity style={style.button} 
-        // onPress={() => {navigator.openChallenges()(props={randomChallenge})}}
-        onPress={() => {
-          console.log('props',props);
-          navigation.navigate("Challenge", {
-            screen: "Challenge",
-            params: props,
-          });
-        }}
+      </View>
+      <View style={[{ display: "flex", flexDirection: "row" }]}>
+        <View
+          style={[
+            style.cardContainer,
+            { backgroundColor: "#rgba(51, 153, 102, 0.7)", width: "48%", marginRight: "1.52%" },
+          ]}
         >
-          <Text style={style.buttonText}>Accept Challenge</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-<View style={[{display:"flex" ,flexDirection:"row"}]}>
+          <TouchableOpacity
+            onPress={() => {
+              console.log("props", props);
+              navigation.navigate("Marketplace", {
+                screen: "Marketplace",
+                params: props,
+              });
+            }}
+          >
+            <View style={style.cardDivider} />
+            <View style={style.container}>
+              <Text style={style.challengeText}>Coin Balance 🪙</Text>
+              <Text style={[{ fontSize: 50 }]}>{coins}</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={[
+            style.cardContainer,
+            { backgroundColor: "#rgba(51, 153, 102, 0.7)", width: "48%", marginLeft: "1.6%" },
+          ]}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              console.log("props", props);
+              navigation.navigate("Coupons", {
+                screen: "Coupons",
+                params: props,
+              });
+            }}
+          >
+            <View style={style.cardDivider} />
+            <View style={style.container}>
+              <Text style={style.challengeText}>All Coupons 🏷️</Text>
 
-<View style={[style.cardContainer, { backgroundColor: '#rgba(51, 153, 102, 0.7)' ,width:'48%' ,marginRight:"1.52%"}]}>
-      <TouchableOpacity
-      onPress={() => {
-        console.log('props',props);
-        navigation.navigate("Marketplace", {
-          screen: "Marketplace",
-          params: props,
-        });
-      }}>
-      <View style={style.cardDivider} />
-      <View style={style.container}>
-        <Text style={style.challengeText}>Coin Balance 🪙</Text>
-        <Text style={[{fontSize:50}]}>{coins}</Text>
-      </View>
-        </TouchableOpacity>
-</View>
-<View style={[style.cardContainer, { backgroundColor: '#rgba(51, 153, 102, 0.7)' ,width:'48%',marginLeft:"1.6%" }]}>
-      <TouchableOpacity
-      onPress={() => {
-        console.log('props',props);
-        navigation.navigate("Coupons", {
-          screen: "Coupons",
-          params: props,
-        });
-      }}>
-      <View style={style.cardDivider} />
-      <View style={style.container}>
-        <Text style={style.challengeText}>All Coupons 🏷️</Text>
+              <Text style={[{ fontSize: 50 }]}>{couponCount}</Text>
 
-        <Text style={[{fontSize:50}]}>{couponCount}</Text>
-
-
-        {/* <TouchableOpacity style={style.button} 
+              {/* <TouchableOpacity style={style.button} 
         // onPress={() => {navigator.openChallenges()(props={randomChallenge})}}
         onPress={() => {
           console.log('props',props);
@@ -252,13 +319,12 @@ const BudgetScreen: NavStatelessComponent = () => {
         >
           <Text style={style.buttonText}>Accept Challeng</Text>
         </TouchableOpacity> */}
+            </View>
+          </TouchableOpacity>
+        </View>
       </View>
-      </TouchableOpacity>
-    </View>
-</View>
-      
-   
-    {/* <Text style={{ fontSize: 20, color: "red", textAlign: "center" }}>{stepCount}</Text> */}
+
+      {/* <Text style={{ fontSize: 20, color: "red", textAlign: "center" }}>{stepCount}</Text> */}
       <ProgressChart
         isMonth
         totalEmissions={totalCurrentMonthEmissions}
@@ -294,13 +360,12 @@ const BudgetScreen: NavStatelessComponent = () => {
         customEmissions={customCurrentYearEmissions}
         monthlyEmissionsBudget={monthlyCarbonBudget}
       />
-      <Leaderboard data={leaderboardData}/>
+      <Leaderboard data={leaderboardData} />
     </ScrollView>
   );
 };
 
 BudgetScreen.navigationOptions = navigationOptions();
-
 
 const style = StyleSheet.create({
   cardContainer: {
@@ -310,132 +375,113 @@ const style = StyleSheet.create({
     marginHorizontal: 0,
     // marginTop: 16,
     elevation: 3,
-    marginTop:6,
-    paddingTop:10
+    marginTop: 6,
+    paddingTop: 10,
   },
   cardTitle: {
     fontSize: 28,
-    fontWeight: 'bold',
-    
-    textAlign:"center",
-    textAlignVertical:"center",
-    padding:5,
-    fontFamily:"Roboto",
-    color:"#ffffff"
+    fontWeight: "bold",
+
+    textAlign: "center",
+    textAlignVertical: "center",
+    padding: 5,
+    fontFamily: "Roboto",
+    color: "#ffffff",
   },
   cardDivider: {
     height: 1,
-    backgroundColor: '#cccccc',
+    backgroundColor: "#cccccc",
     marginBottom: 16,
   },
   container: {
-    alignItems: 'center',
+    alignItems: "center",
   },
   challengeText: {
     marginBottom: 10,
     fontSize: 18,
   },
   button: {
-    backgroundColor: '#007bff',
+    backgroundColor: "#007bff",
     borderRadius: 8,
     paddingHorizontal: 16,
     paddingVertical: 8,
   },
   buttonText: {
-    color: '#ffffff',
+    color: "#ffffff",
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
 
-
 const styleDailyCheckin = StyleSheet.create({
-
   superContainer: {
-    backgroundColor: '#E0C350',
+    backgroundColor: "rgba(51, 153, 102, 0.7)",
     marginVertical: 3,
-    marginHorizontal:0,
+    marginHorizontal: 0,
     paddingVertical: 16,
-    paddingHorizontal:5,
+    paddingHorizontal: 5,
     borderRadius: 8,
-    paddingTop:10
+    paddingTop: 10,
   },
-  Daily:{
-    textAlign:"center",
-    textAlignVertical:"center",
-    padding:5,
-    fontSize:20,
-    fontWeight:"bold",
-    fontFamily:"Roboto",
-    color:"#ffffff"
+  Daily: {
+    textAlign: "center",
+    textAlignVertical: "center",
+    padding: 5,
+    fontSize: 20,
+    fontWeight: "bold",
+    fontFamily: "Roboto",
+    color: "#ffffff",
   },
   container: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    // backgroundColor: ' #000000',
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   dayContainer: {
-    alignItems: 'center',
+    alignItems: "center",
   },
   day: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
-  tick: {
-    fontSize: 24,
-    color: 'green',
+  tickContainer: {
+    backgroundColor: "rgba(51, 153, 102, 0.7)",
   },
-  cross: {
-    fontSize: 24,
-    color: 'red',
+  crossContainer: {
+    backgroundColor: "rgba(255, 51, 51, 0.7)",
   },
   statusContainer: {
     width: 36,
     height: 36,
     borderRadius: 18,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  tickContainer: {
-    backgroundColor: 'rgba(51, 153, 102, 0.7)', // Greenish background
-  },
-  crossContainer: {
-    backgroundColor: 'rgba(255, 51, 51, 0.7)', // Light red background
+    alignItems: "center",
+    justifyContent: "center",
   },
   statusText: {
     fontSize: 24,
-    color: '#ffffff',
+    color: "#ffffff",
   },
   checkInPoints: {
     fontSize: 18,
     marginTop: 4,
-    color: '#ffffff',
-    fontFamily: 'Roboto',
+    color: "#ffffff",
+    fontFamily: "Roboto",
   },
-  checkInPointsContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+  streak: {
+    fontSize: 14,
+    color: "#888888",
     marginTop: 4,
+    fontFamily: "Roboto",
+    textAlign: "center",
+  },
+  finalStreak: {
+    marginTop: 10,
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#ffffff",
+    textAlign: "center",
   },
 });
-
-
-const sty = StyleSheet.create({
-  container: {
-    backgroundColor: '#dcdcdc', // Grey background color
-    padding: 14,
-    borderRadius: 8,
-    marginHorizontal:1,
-  },
-  innerContainer: {
-    backgroundColor: '#00FFFF', // Inner container background
-    borderRadius: 8,
-    padding: 16,
-  },
-});
-
-
-
 
 export default BudgetScreen;
